06d656e32e03eb621e332fcd7234ad32f359d906
wasim ullah 209 <wasimcs88@gmail.com>
2025-04-24 05:05:05 +0500

    A new file is added for the first program of python

---
+++ firstprog.py
@@ -0,0 +1,22 @@
+# printing the hello world
+print ("hello world")
+
+# variables in python:- variables are name given to a memory location in a program 
+# let practice some variable 
+
+name = "wasim ullah khan"
+age = 20
+semester = 6
+prog = "BS"
+subject = "Computer Science"
+uni = "Islamia College University Peshawar"
+
+# now printing the above variables
+print("name is :" , name)
+print("Age is : ", age)
+print("Current Semester :" , semester)
+print("Program is :" , prog)
+print("Subject is :" , subject)
+print("University name is :" , uni)
+
+
