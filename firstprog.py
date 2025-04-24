a9018869846c4ca42c6ca7fcc654fc26bd46987b
wasim ullah 209 <wasimcs88@gmail.com>
2025-04-24 22:49:31 +0500

    adding also the point in the feature1 from main

---
+++ firstprog.py
@@ -0,0 +1,27 @@
+# printing the hello world
+print ("hello world")
+
+# variables in python:- variables are name given to a memory location in a program 
+# let practice some variable 
+
+name = "wasim ullah khan"
+age = 20
+semester = 6
+prog = "BS"
+subject = "Computer Science"
+uni = "Islamia College University Peshawar"
+
+# now printing the above variables
+print("name is :" , name)
+print("Age is : ", age)
+print("Current Semester :" , semester)
+print("Program is :" , prog)
+print("Subject is :" , subject)
+print("University name is :" , uni)
+
+# adding two vlaues in python
+
+a = 16
+b = 19
+c = a+b
+print ("the sum of a and b is :" , c)
