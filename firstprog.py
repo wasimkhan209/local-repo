7571688f0ae51ca7f1be280e909529e7e623b805
wasim ullah 209 <wasimcs88@gmail.com>
2025-04-25 15:39:30 +0500

    adding a new method of print the inputs

--- firstprog.py
+++ firstprog.py
@@ -1,11 +1,17 @@
-# input String
-print ("Enter your Good Name")
-name = input ("")
+# # input String
+# print ("Enter your Good Name")
+# name = input ("")
 
-# Enter Your Age
-print("Enter your Age: ")
-age = int(input(""))
+# # Enter Your Age
+# print("Enter your Age: ")
+# age = int(input(""))
 
-# Now print the name and age
-print ("your Good Name is :" , name)
-print ("your age is : " , age)
+# # Now print the name and age
+# print ("your Good Name is :" , name)
+# print ("your age is : " , age)
+
+name = input("enter your name :")
+age = int(input("enter your age :"))
+price = float(input("enter the price:"))
+
+print ("My Name is ", name , "and i'm " , age , "years old.")
