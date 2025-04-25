cb6490ff30c61d7a858702536917a382ea0ca436
wasim ullah 209 <wasimcs88@gmail.com>
2025-04-25 15:31:27 +0500

    adding the int input

--- firstprog.py
+++ firstprog.py
@@ -1,3 +1,11 @@
+# input String
 print ("Enter your Good Name")
 name = input ("")
+
+# Enter Your Age
+print("Enter your Age: ")
+age = int(input(""))
+
+# Now print the name and age
 print ("your Good Name is :" , name)
+print ("your age is : " , age)
