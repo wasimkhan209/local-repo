460b78a07a194e9dd201dc05d32cc7782abf12a9
wasim ullah 209 <wasimcs88@gmail.com>
2025-04-26 21:51:14 +0500

    ADDING THE QUAIZ IN THE SECOND CHAPTER AND THIS IS THE END OF CHAP #02

--- second_chapter.py
+++ second_chapter.py
@@ -21,3 +21,18 @@ str = str.capitalize()
 print(str)
 print(str.endswith("nce")) # True
 print(str.endswith("eg")) #False
+
+#  write a Prog that input four vlaues and find the Gretest one
+
+a = float (input("Enter the First Number : "))
+b = float (input("Enter the Second Number : "))
+c = float (input("Enter the Third Number : "))
+d = float (input("Enter the Fourth Number : "))
+if(a >=b and a>= b and a >= c and a >= d):
+    print("the first vlaue you have entered is the Greatest")
+elif(b >=c and b >=d):
+    print("The Second Value you have entered is Greatest")
+elif(c >= d):
+    print("Third Vlaue is Greatest ")
+else:
+    print("fourth Vlaue you have Entered is greatest")
