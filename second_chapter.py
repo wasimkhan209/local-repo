cb867ff74c0a49ec9a519b09275d2e5dc29ea632
wasim ullah 209 <wasimcs88@gmail.com>
2025-04-26 18:45:30 +0500

    SECOND CHAPTER

---
+++ second_chapter.py
@@ -0,0 +1,23 @@
+# Methods of creating Strings
+# name = "wasim"
+# name = 'wasim'
+# name = '''wasim'''
+# print(name)
+
+# Indexing
+# print(name[0:3])
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+# OR
+# print(name[-5:len(name)])
+
+str = "i am wasim khan and im studying BS computer science"
+# print(str.capitalize())
+str = str.capitalize()
+print(str)
+print(str.endswith("nce")) # True
+print(str.endswith("eg")) #False
